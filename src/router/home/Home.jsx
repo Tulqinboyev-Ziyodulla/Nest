516b98055d5d66cb2101433b23736221a274ae6b
Tulqinboyev-Ziyodulla <ziyodulla464@gmail.com>
2024-08-23 09:53:28 +0500

    has a drawback

--- src/router/home/Home.jsx
+++ src/router/home/Home.jsx
@@ -4,20 +4,18 @@ import Banner from "../../components/banner/Banner";
 import Stay from "../../components/stay/Stay";
 import Pricing from "../../components/pricing/Pricing";
 import Product from "../../components/product/Product";
-import Swipers from "../../components/swiper/Swiper";
 import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
-  // return (
-  //   <>
-  //   <Swipers />
-  //   <Featured />
-  //   <Banner />
-  //   <Product />
-  //   <Stay />
-  //   <Pricing />
-  //   </>
-  // );
+  return (
+    <>
+    <Featured/>
+    <Banner/>
+    <Product/>
+    <Stay/>
+    <Pricing/>
+    </>
+  )
 };
 
 export default Home;
